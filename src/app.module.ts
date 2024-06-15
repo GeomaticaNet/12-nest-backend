460718d0550d3e8ca2670acf9056c09033163038
Matias Sendra <mjs.sendra@gmail.com>
2024-06-15 19:05:50 -0300

    Commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URI),
+
+    AuthModule,
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+  constructor() {
+    console.log(process.env);
+  }
+}
