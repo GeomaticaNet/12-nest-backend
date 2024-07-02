4d3f693600ffd6db4072bb25c1de064a49836e64
Matias Sendra <mjs.sendra@gmail.com>
2024-07-02 00:42:47 -0300

    final commit2

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
     ConfigModule.forRoot(),
 
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
 
     AuthModule,
 
